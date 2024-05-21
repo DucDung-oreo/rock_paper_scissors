1c1c11292bba003072109e0777688e555eaeabce
DucDung-oreo <huynhducdung1399@gmail.com>
2024-05-21 23:52:10 +0700

    Add another window to test the connection
    	Focus on the log
    	Why can't we get the same log

--- main.cpp
+++ main.cpp
@@ -5,6 +5,11 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    w.setWindowTitle("Player 1");
     w.show();
+
+    MainWindow test_window;
+    test_window.setWindowTitle("Player 2");
+    test_window.show();
     return a.exec();
 }
